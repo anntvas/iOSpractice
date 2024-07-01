1789d1e86a533529c0d7eb5205750934ac764f45
anntvas <vasilevasugai@gmail.com>
2024-07-01 09:12:24 +0300

    Initial Commit

---
+++ hwiOS/hwiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  hwiOSApp.swift
+//  hwiOS
+//
+//  Created by Anna on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct hwiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
